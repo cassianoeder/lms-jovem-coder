36ee8a4792a2756074a8055ac4917942341e7c8c
[dyad] <git@dyad.sh>
2025-12-08 20:06:51 -0300

    Reverted all changes back to version 2d176e15541f91f04eab8cd63498a154c2acb65e

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// Export ReactDOM for use in certificate generation utility
-export { createRoot };
-
 createRoot(document.getElementById("root")!).render(<App />);
